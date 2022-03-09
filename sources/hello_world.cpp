4b8283e51bb0228f2582233de7b147d9f7a99e22
keyow <kirill500123@gmail.com>
2022-03-09 09:08:08 +0300

    getting username

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "Hello world " << endl;
+	string name;
+	getline(cin, name);
+	cout << "Hello world " << name << endl;
 	return 0;
 }
