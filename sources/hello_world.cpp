cec92a605b397ee12d974c8e6161d3682d9816ad
Kirill Rabtsevich <59114186+keyow@users.noreply.github.com>
2022-03-09 10:18:52 +0300

    changed comments language to russian

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-	std::string name; // username
-	std::getline(std::cin, name); // getting username
-	std::cout << "Hello world " << std::name << std::endl; // output
+	std::string name; // Имя пользователя
+	std::getline(std::cin, name); // Ввод
+	std::cout << "Hello world " << std::name << std::endl; // Вывод
 	return 0;
 }
