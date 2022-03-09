24913113a3c6671acb88df1bb2765d007f7d9b6b
keyow <kirill500123@gmail.com>
2022-03-09 09:27:23 +0300

    added comments

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-	std::string name;
-	std::getline(std::cin, name);
-	std::cout << "Hello world " << std::name << std::endl;
+	std::string name; // username
+	std::getline(std::cin, name); // getting username
+	std::cout << "Hello world " << std::name << std::endl; // output
 	return 0;
 }
